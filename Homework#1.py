300263c234dd629d10f67d003f83afc1350618d9
You1104419127 <JianNing.Luo@mail.citytech.cuny.edu>
2023-02-14 18:30:48 -0500

    first assignment

---
+++ Homework#1.py
@@ -0,0 +1,7 @@
+print("enter your time here")
+t = input()
+t = int(t)
+height = 1/2 * 9.8 * t**2
+print("you ball was at rest and was falling from")
+print(height)
+print("meters above the ground")
