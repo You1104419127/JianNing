f656e045100439e7bcbfdf73c6dc35e9fbe23a15
You1104419127 <JianNing.Luo@mail.citytech.cuny.edu>
2023-02-18 11:22:56 -0500

    good file

--- CurvePlotting1a.py
+++ CurvePlotting1a.py
@@ -1,12 +1,11 @@
 import numpy as np
 from matplotlib import pyplot as plt
-# (2b):
-theta = np.arange(0, 10*np.pi, 0.1)
-r = theta**2
-x = r*np.cos(theta)
-y = r*np.sin(theta)
+# (3c):
+theta = np.arange(0, 2*np.pi, 0.1)
+x = 2*np.cos(theta) + np.cos(2*theta)
+y = 2*np.sin(theta) - np.sin(2*theta)
 plt.figure(figsize=(8,5))
 ax = plt.axes()
-ax.set(xlabel='x', ylabel='y',title='Galilean spiral')
+ax.set(xlabel='x', ylabel='y',title='deltoid curve')
 plt.plot(x,y)
 plt.show()
