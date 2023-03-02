e99963630cdb7527382fd36781e6ce88a5fc2d70
jianning <124749303+You1104419127@users.noreply.github.com>
2023-03-02 13:36:20 -0500

    Update EnhancedHW#1.py

--- EnhancedHW#1.py
+++ EnhancedHW#1.py
@@ -5,3 +5,4 @@ t = int(sys.argv[1])
 height = 1/2 * 9.8 * t**2
 
 print("You ball was at rest and was falling at", height, "meters above the ground")
+# for the file name, we should avoid all special characters
