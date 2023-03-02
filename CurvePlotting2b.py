a395b4f919ec4ee73f17cc1333995851f426b814
You1104419127 <JianNing.Luo@mail.citytech.cuny.edu>
2023-03-02 16:33:50 -0500

    CurvePlotting2b

--- CurvePlotting2b.py
+++ CurvePlotting2b.py
@@ -1,12 +1,12 @@
 import numpy as np
 from matplotlib import pyplot as plt
-# (3c):
-theta = np.arange(0, 24*np.pi, 0.1)
-r = np.e**(np.cos(theta)) - 2*np.cos(4*theta) + (np.sin(theta/12))**2
+# (3b):
+theta = np.arange(0, 10*np.pi, 0.1)
+r = theta**2
 x = r*np.cos(theta)
 y = r*np.sin(theta)
 plt.figure(figsize=(8,5))
 ax = plt.axes()
-ax.set(xlabel='x', ylabel='y',title='Fey’s function')
+ax.set(xlabel='x', ylabel='y',title='Galilean spiral')
 plt.plot(x,y)
 plt.show()
