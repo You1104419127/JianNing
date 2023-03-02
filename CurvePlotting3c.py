3cf17c31dfc27a28842f9b0a77b80cc8ed732de6
jianning <124749303+You1104419127@users.noreply.github.com>
2023-03-02 16:55:56 -0500

    Update CurvePlotting3c.py

--- CurvePlotting3c.py
+++ CurvePlotting3c.py
@@ -2,7 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 # (3c):
 theta = np.arange(0, 24*np.pi, 0.1)
-r = np.e**(np.cos(theta)) - 2*np.cos(4*theta) + (np.sin(theta/12))**2
+r = exp(np.cos(theta)) - 2*np.cos(4*theta) + (np.sin(theta/12))**2
 x = r*np.cos(theta)
 y = r*np.sin(theta)
 plt.figure(figsize=(8,5))
